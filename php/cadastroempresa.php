278afcc98714df14a8f59102daf97a1ee5c206cd
febzr <136595371+febzr@users.noreply.github.com>
2023-09-22 22:36:14 -0300

    MODIFICAÇÃO 22/09 - fbzr
    Adição de login para ambos;
    Alterações em arquivos do frontend para adicionar funcionalidade a botões existentes de cadastro e recuperação de senha.

--- php/cadastroempresa.php
+++ php/cadastroempresa.php
@@ -12,5 +12,6 @@ $senha_hashed = password_hash($senha, PASSWORD_DEFAULT);
 $sql = "INSERT INTO empresas (nome, cnpj, email, senha) VALUES ('$nome', '$cnpj', '$email', '$senha_hashed')";
 
 mysqli_query($conn, $sql);
+header('Location: ../login-empresa.html');
 
 ?>
---
+++ php/loginempresa.php
@@ -0,0 +1,25 @@
+<?php
+include 'db.php';
+
+$cnpj = $_POST['cnpj'];
+$senha = $_POST['senha'];
+
+// Busca a senha do banco de dados
+$sql = "SELECT senha FROM empresas WHERE cnpj = '$cnpj'";
+$result = mysqli_fetch_array(mysqli_query($conn, $sql));
+$senha_fromdb = $result['senha'];
+
+// Verifica se a senha está certa
+$senha_decrypted = password_verify($senha, $senha_fromdb);
+echo $senha_decrypted;
+
+// Retorna o resultado para a página
+if($senha_decrypted == '1'){
+    session_start(); //Inicia a sessão
+    $_COOKIE['cnpj'] = $cnpj; //Monta o cookie CNPJ
+    header('Location: ../upconsult_index.html');
+
+} else {
+    // erro de login vai aqui
+}
+?>
