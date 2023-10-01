7b848f925a7b1c9c97294468933ee98ce118298d
febzr <136595371+febzr@users.noreply.github.com>
2023-09-30 21:54:25 -0300

    Alterações - 30/09
    Alteração da página de login;
    Alteração de funções diversas e adição de partes do backend ao código principal;
    Adição de verificação de existência de usuário e senha

---
+++ recuperacao-senha.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <!--
+        FONTES:
+            font-family: 'Inter', sans-serif;
+            font-family: 'Montserrat', sans-serif;
+    -->
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <!--
+        Arquivos locais
+    -->
+    <link rel="icon" type="image/png" href="./Resources/Plataforma/geral-logo-amarela.png">
+    <link rel="stylesheet" href="./recup-senha-style.css">
+    <script src="./script.js"></script>
+
+
+    <title>Recuperação de senha</title>
+
+</head>
+
+<body>
+
+    <header>
+        <div class="sup-recup-senha">
+            <img src="Resources/Cadastro e Login images/logo-superior-direito.png" alt="">
+        </div>
+        </div>
+        <div class="seta-senha">
+            <a href=""><img src="Resources/Cadastro e Login images/conf-icon-seta-voltar.png" alt=""></a>
+        </div>
+        <div class="escuro-senha">
+            <ul>
+                <li><a href=""><img src="Resources/Cadastro e Login images/icon-escuro.png" alt=""></a>
+                </li>
+                <li><a href=""><img src="Resources/Cadastro e Login images/icon-faq.png" alt=""></a>
+                </li>
+            </ul>
+        </div>
+    </header>
+    <main>
+        <section>
+            <h1>Recuperação de<br>Senha</h1>
+            <p>Esqueceu a senha? Sem problemas!<br>Insira seu e-mail e te enviamos o link<br>de recuperaçção.</p>
+            <form action="">
+                <input type="text" placeholder="Digite seu e-mail"><br>
+                <input type="submit" name="" id="recuperar" value="RECUPERAR SENHA">
+            </form>
+        </section>
+        <section class="i-senha">
+            <img src="Resources/Cadastro e Login images/rec-senha-image.png" alt="">
+        </section>
+        <div class="i-logo-senha">
+            <img src="Resources/Cadastro e Login images/logo-footer.png" alt="">
+        </div>
+    </main>
+    <footer>
+        <img src="Resources/Cadastro e Login images/detalhe-inferior-esquerdo.png" alt="">
+    </footer>
+
+</body>
+
+</html>
