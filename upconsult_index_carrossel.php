410a28c5391e1c1e0373dab6776d128b42778b03
khadidjamoraes <khadidja.developer@gmail.com>
2023-10-02 19:22:33 -0300

    Create upconsult_index_carrossel.php

---
+++ upconsult_index_carrossel.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upconsult</title>
+
+    <!--
+        FONTES:
+            font-family: 'Inter', sans-serif;
+            font-family: 'Montserrat', sans-serif;
+        CORES PRINCIPAIS:
+            azul: #121276
+            amarelo: #E5C73F
+            cinza claro: #D9D9D9
+            cinza escuro: #1E1E26
+            verde: #0B9F08
+    -->
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+
+    <link rel="icon" type="image/png" href="./Resources/Plataforma/geral-logo-amarela.png">
+    <link rel="stylesheet" href="upconsult_index_solicitacao_style.css">
+</head>
+
+<body>
+
+    <!-- Menu principal -->
+    <header class="menu-principal">
+        <figure class="imagem-logo">
+            <img src="./Resources/Plataforma/geral-logo-amarela.png" alt="">
+        </figure>
+        <nav id="menu-opcoes">
+            <ul>
+                <li class="icone-menu mensagens"><a href="">
+                        <img src="./Resources/Plataforma/geral-menu-mensagens.png" alt="">
+                    </a></li>
+                <li class="icone-menu agenda"><a href="">
+                        <img src="./Resources/Plataforma/geral-menu-agenda.png" alt="">
+                    </a></li>
+                <li class="icone-menu notificacoes"><a href="">
+                        <img src="./Resources/Plataforma/geral-menu-notificacoes.png" alt="">
+                    </a></li>
+                <li class="icone-menu-config"><a href="">
+                        <img src="./Resources/Plataforma/geral-menu-configs.png" alt="">
+                    </a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <!-- Área de notificações empresa -->
+
+        <main class="carousel-container">
+            <div class="carousel">
+                <ul class="carousel-list">
+                    <li class="carousel-item">
+                        <h3>Título do Card 1</h3>
+                        <p>Descrição do Card 1</p>
+                        <p>Tipo: Tipo do Card 1</p>
+                    </li>
+                    <!-- Adicione mais cards aqui -->
+                </ul>
+            </div>
+            <button class="prev-button">Anterior</button>
+            <button class="next-button">Próximo</button>
+        </main>
+    <script src="carrossel.js"></script>
+        
+</body>
+
+</html>
