662429f002dde6aa6718ea97e21f5eb61d25f7ff
khadidjamoraes <khadidja.developer@gmail.com>
2023-10-02 19:22:47 -0300

    Create upconsult_index_empresa.php

---
+++ upconsult_index_empresa.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upconsult</title>
+
+    <!--
+        FONTES:
+            font-family: 'Inter', sans-serif;
+            font-family: 'Montserrat', sans-serif;
+        CORES PRINCIPAIS:
+            azul: #121276
+            amarelo: #E5C73F
+            cinza claro: #D9D9D9
+            cinza escuro: #1E1E26
+            verde: #0B9F08
+    -->
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+
+    <link rel="icon" type="image/png" href="./Resources/Plataforma/geral-logo-amarela.png">
+    <link rel="stylesheet" href="upconsult_index_style.css">
+</head>
+
+<body>
+
+    <!-- Menu principal -->
+    <header class="menu-principal">
+        <figure class="imagem-logo">
+            <img src="./Resources/Plataforma/geral-logo-amarela.png" alt="">
+        </figure>
+        <nav id="menu-opcoes">
+            <ul>
+                <li class="icone-menu mensagens"><a href="">
+                        <img src="./Resources/Plataforma/geral-menu-mensagens.png" alt="">
+                    </a></li>
+                <li class="icone-menu agenda"><a href="">
+                        <img src="./Resources/Plataforma/geral-menu-agenda.png" alt="">
+                    </a></li>
+                <li class="icone-menu notificacoes"><a href="">
+                        <img src="./Resources/Plataforma/geral-menu-notificacoes.png" alt="">
+                    </a></li>
+                <li class="icone-menu-config"><a href="">
+                        <img src="./Resources/Plataforma/geral-menu-configs.png" alt="">
+                    </a></li>
+            </ul>
+        </nav>
+    </header>
+
+
+    <!-- Parte principal do consultor -->
+    <main class="principal-consultor">
+        <section class="info-principais">
+            <div class="info-principais-card">
+                <figure class="info-principais-foto-perfil">
+                    <img src="./Resources/Plataforma/new.png" alt=""
+                        id="info-principais-foto-consultor">
+                </figure>
+                <div>
+                    <p class="info-principais-nome-consultor"><strong>Nome da Empresa</strong></p>
+                    <p class="info-principais-localizacao-consultor">Localização da empresa</p>
+                </div>
+            </div>
+            <div 
+            class="info-principais-card-atendimento-consultor">
+                <img src="./Resources/Plataforma/geral-atendimento.png" alt="">
+            </div>
+            
+            <button class="info-principais-criar-feed-consultor" type="button" onclick="window.location.href='upconsult_index_solicitacao.html'">
+                <img src="./Resources/Plataforma/empresa-realizar-solicitacao.png" alt="">
+            </button>
+        </section>
+
+    </main>
+
+
+    <script src="./upconsult-script.js" type="js"></script>
+</body>
+
+</html>
